9574b37ef8669591a81bd4477aeb61d56f55e1d5
Santosh2167 <santosh.poudyal@gmail.com>
2018-09-09 17:39:40 -0700

    first commit

---
+++ array.rb
@@ -0,0 +1,19 @@
+shoppingList = ["cheese","toilet paper","taco shells"]
+#puts "i really like #{myarray[2]}"
+
+#puts shoppingList
+# i = 0
+# while i<shoppingList.length do 
+#     puts "i = #{i}"
+#     puts "shopping list item: #{shoppingList[i]}"
+#     i +=1
+# end 
+
+# shoppingList.each do |item|
+
+#     puts "Shopping list item: #{item}"
+# end
+
+for item in shoppingList
+    puts "Buy this: #{item}"
+end
